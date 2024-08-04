69256c7badc0d21a6619c160801a403806be5a15
MrEpoch <alexandrstencuk@seznam.cz>
2024-08-05 00:04:45 +0200

    feat: Updated styling

--- src/app/design/[type]/[id]/page.tsx
+++ src/app/design/[type]/[id]/page.tsx
@@ -2,25 +2,24 @@ import React from "react";
 import { constDesigns } from "@/constants/cs_main";
 import { redirect } from "next/navigation";
 import Scroller from "../_sections/Scroller";
-import Parallax from "@/components/ui/parallax-scroll";
 import Image from "next/image";
 
 function SliderElement({ sliderContent }: { sliderContent: any }) {
   return (
-    <div className="w-full min-h-screen gap-8 flex items-center justify-center shadow-lg lg:flex-row flex-col">
-      <div className="flex h-full items-center flex-1 justify-center flex-col gap-4 w-full">
+    <div className="w-full max-w-container py-4 min-h-screen gap-8 flex items-center justify-center lg:flex-row flex-col">
+      <div className="flex h-full lg:items-start items-center flex-1 justify-center flex-col gap-4 w-full">
         <h2 className="huge-heading p-0 m-0 text-start">
           {sliderContent.title}
         </h2>
         <p className="text-sm text-main-text-100">{sliderContent.para}</p>
       </div>
-      <div className="w-full h-full flex-1">
+      <div className="w-full h-full flex items-center justify-center flex-1">
         <Image
           src={sliderContent.image}
           alt="hero"
           height={720}
           width={1400}
-          className="w-full h-full object-cover"
+          className="h-fit w-fit object-contain"
         />
       </div>
     </div>
@@ -54,19 +53,28 @@ export default function page({
     <main className="min-h-container scroll-snap-container">
       <div className="max-w-container">
         <Scroller designData={designData} />
-        <div
-          className={`min-h-screen w-full p-8 rounded-lg`}
-          style={{ background: designData.infoSlider.color }}
-        >
-          <div className="flex flex-col gap-4 overflow-y-scroll scroll-snap-container max-h-[700px] h-full p-4">
-            <Parallax
-              elementList={designData.infoSlider.content.map(
-                (sliderContent, i) => (
-                  <SliderElement key={i} sliderContent={sliderContent} />
-                ),
-              )}
-            />
-          </div>
+      </div>
+      <div
+        className={`min-h-full flex flex-col gap-16 w-full p-7 rounded-lg`}
+        style={{ background: designData.infoSlider.color }}
+      >
+        {designData.infoSlider.content.map((sliderContent, i) => (
+          <SliderElement key={i} sliderContent={sliderContent} />
+        ))}
+      </div>
+      <div className="w-full relative bg-main-background-100 min-h-[300px]">
+        <div className="custom-shape-divider-top-1722808597">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+              style={{ fill: designData.infoSlider.color }}
+            ></path>
+          </svg>
         </div>
       </div>
     </main>
